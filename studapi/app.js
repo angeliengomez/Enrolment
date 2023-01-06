0fdfc81afccc347d0ba35b3aea20ba1479d60bd2
Angelien Gomez <gomezangelien@gmail.com>
2023-01-06 09:43:02 +0800

    MongoUpdate

--- studapi/app.js
+++ studapi/app.js
@@ -1,6 +1,7 @@
 const koa = require('koa');
 const cors = require('@koa/cors');
 const {koaBody} = require('koa-body');
+const bodyParser = require('koa-bodyparser');
 
 
 const app = new koa();
@@ -12,11 +13,12 @@ app.use(cors());
 app.use(stud.routes()).use(stud.allowedMethods());
 
 app.listen(3001, function() {
-    console.log("Server Started At : http://localhost:30001/students");
+    console.log("Server Started At : http://localhost:3001/students");
 });
 
 const mongo = new koa();
 
+mongo.use(bodyParser());
 const mongostud = require('./mongorouter');
 
 mongo.use(cors());
--- studapi/mongorouter.js
+++ studapi/mongorouter.js
@@ -1,5 +1,5 @@
 const Router = require('koa-router');
-const { getAllStudents } = require('./Mongo/MongoDatabase');
+const { getAllStudents, insertStudent, updateStudent, deleteStudent } = require('./Mongo/MongoDatabase');
 
 const router = new Router({
     prefix: '/students'
@@ -11,5 +11,71 @@ router.get('/',(ctx, next) => {
     console.log("Get All Student Data")
     next();
 });
+
+//insert
+router.post('/insert', async (ctx, next)=>{
+    console.log("Router Data : " + ctx.request.body);
+    if(!ctx.request.body.Lastname || !ctx.request.body.Firstname || !ctx.request.body.Course)
+    {
+        ctx.response.status = 404;
+        ctx.body = "Missing fields";
+    }
+    else
+    {
+        var res = await insertStudent(ctx.request.body.Lastname, ctx.request.body.Firstname, ctx.request.body.Course);
+        if(res)
+        {
+            console.log("Success Insert");
+            ctx.body = "Successfully Added Student";
+            ctx.response.status = 201;
+        }
+    }
+    next();
+});
+
+//update
+
+router.post('/update', async (ctx, next) => {
+    if(!ctx.request.body.Lastname || !ctx.request.body.Firstname || !ctx.request.body.Course)
+    {
+        console.log("Missing Fields");
+        ctx.response.status = 404;
+        ctx.body = "Missing fields";
+    }
+    else
+    {
+        var res = await updateStudent(ctx.request.body.Lastname, ctx.request.body.Firstname, ctx.request.body.Course);
+        if(res)
+        {
+            ctx.response.status = 201;
+            ctx.body = "Successful Update";
+            console.log("Success Update");
+        }
+
+    }
+    next();
+});
+
+//delete
+
+router.post('/delete', async (ctx, next) =>{
+    if(!ctx.request.body.id)
+    {
+        ctx.response.status = 404;
+        ctx.body = "Missing Fields";
+    }
+    else
+    {
+        console.log(ctx.request.body.id);
+        var res = await deleteStudent(ctx.request.body.id);
+        if(res)
+        {
+            ctx.response.status = 201;
+            ctx.body = "Account Deleted";
+            console.log("Account Deleted");
+        }
+    }
+    next();
+})
 
 module.exports = router;
