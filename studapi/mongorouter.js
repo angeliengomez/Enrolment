04b06ffc81f4fa1f661bcde0b258c73053581a7b
RenSoon <sbrendonsamson@gmail.com>
2023-01-20 20:23:32 +0800

    my commit

--- studapi/mongorouter.js
+++ studapi/mongorouter.js
@@ -34,38 +34,10 @@ router.post('/insert', async (ctx, next) =>{
     next();
 });
 
-<<<<<<< HEAD
-//insert
-router.post('/insert', async (ctx, next)=>{
-    console.log("Router Data : " + ctx.request.body);
-    if(!ctx.request.body.Lastname || !ctx.request.body.Firstname || !ctx.request.body.Course)
-    {
-        ctx.response.status = 404;
-        ctx.body = "Missing fields";
-    }
-    else
-    {
-        var res = await insertStudent(ctx.request.body.Lastname, ctx.request.body.Firstname, ctx.request.body.Course);
-        if(res)
-        {
-            console.log("Success Insert");
-            ctx.body = "Successfully Added Student";
-            ctx.response.status = 201;
-        }
-    }
-    next();
-});
-
 //update
-
-router.post('/update', async (ctx, next) => {
-    if(!ctx.request.body.Lastname || !ctx.request.body.Firstname || !ctx.request.body.Course)
-=======
-//Update Document
 router.post('/update', async (ctx, next) => {
     console.log(ctx.request.body);
     if(!ctx.request.body.id || !ctx.request.body.FirstName || !ctx.request.body.LastName || !ctx.request.body.Course)
->>>>>>> 431b696b4e85d03e753b13adbd88d1f9eda7cd87
     {
         console.log("Missing Fields");
         ctx.response.status = 404;
@@ -73,11 +45,7 @@ router.post('/update', async (ctx, next) => {
     }
     else
     {
-<<<<<<< HEAD
-        var res = await updateStudent(ctx.request.body.Lastname, ctx.request.body.Firstname, ctx.request.body.Course);
-=======
         var res = await updateAccount(ctx.request.body.id, ctx.request.body.FirstName, ctx.request.body.LastName,  ctx.request.body.Course);
->>>>>>> 431b696b4e85d03e753b13adbd88d1f9eda7cd87
         if(res)
         {
             ctx.response.status = 201;
@@ -89,12 +57,7 @@ router.post('/update', async (ctx, next) => {
     next();
 });
 
-<<<<<<< HEAD
-//delete
-
-=======
 //Delete a Document
->>>>>>> 431b696b4e85d03e753b13adbd88d1f9eda7cd87
 router.post('/delete', async (ctx, next) =>{
     if(!ctx.request.body.id)
     {
@@ -104,11 +67,7 @@ router.post('/delete', async (ctx, next) =>{
     else
     {
         console.log(ctx.request.body.id);
-<<<<<<< HEAD
-        var res = await deleteStudent(ctx.request.body.id);
-=======
         var res = await deleteAccount(ctx.request.body.id);
->>>>>>> 431b696b4e85d03e753b13adbd88d1f9eda7cd87
         if(res)
         {
             ctx.response.status = 201;
@@ -119,8 +78,4 @@ router.post('/delete', async (ctx, next) =>{
     next();
 })
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 431b696b4e85d03e753b13adbd88d1f9eda7cd87
